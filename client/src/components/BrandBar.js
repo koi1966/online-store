4ca87a1aee56f524f055ede63be50b760163012e
koi1966 <koi196621@gmail.com>
2023-05-21 21:50:37 +0300

    1.39.38

--- client/src/components/BrandBar.js
+++ client/src/components/BrandBar.js
@@ -6,14 +6,14 @@ import {Card, Row} from "react-bootstrap";
 const BrandBar = observer( ()=> {
     const {device} = useContext(Context)
     return (
-        <Row className='d-flex'>
+        <Row className='ms-auto'>
             {device.brands.map(brand =>
                 <Card
                     style={{cursor:'pointer'}}
                     key={brand.id}
                     className="p-3"
                     onClick={()=> device.setSelectedBrand(brand)}
-                    border={brand.id === device.selectedBrand.id ? 'bander': 'light'}
+                    border={brand.id === device.selectedBrand.id ? 'brander': 'light'}
                 >
                     {brand.name}
                 </Card>
